587ec180d3c184c7065467fee4211e4e9c699489
Gabriel Barrett <gabrielbarrett@dhcp-18-20-228-60.dyn.mit.edu>
2020-01-25 13:06:34 -0500

    added all the sound files and the code to make said files

---
+++ sounds.py
@@ -0,0 +1,29 @@
+from gtts import gTTS 
+from playsound import playsound
+
+language = 'en'
+
+
+def TTF(filename, text):
+        """
+        where text and filename are trings
+        filename = string
+        text = string
+        """
+        voice = gTTS(text=text, lang = language, slow = False)
+        voice.save(filename)
+
+prompts ={}
+prompts['leftClear'] = 'The left side of the vehicle is now clear for exit'
+prompts['leftBlocked'] = 'The left side of the vehicle is not clear for exit'
+prompts['rightClear'] = 'The right side of the vehicle is now clear for exit'
+prompts['rightBlocked'] = 'The right side of the vehicle is not clear for exit'
+
+for filename in prompts:
+    name  = filename + '.mp3'
+    # TTF(name, prompts[filename])
+    playsound(name)
+    break
+
+
+
