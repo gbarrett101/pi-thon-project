953526845c7341a7ee984c2ac07f5b7fe2efed21
Gabriel Barrett <gabrielbarrett@dhcp-18-21-148-176.dyn.mit.edu>
2020-01-27 18:30:40 -0500

    fixed loop and openned port

--- serialTest.py
+++ serialTest.py
@@ -2,14 +2,17 @@ import serial
 import time
  
 s = serial.Serial('/dev/ttyUSB0', 9600) # change name, if needed
-# s.open()
+s.open()
 time.sleep(5) # the Arduino is reset after enabling the serial connection, therefore we have to wait some seconds
 response = ""
-while True:
-    new_response = s.readline()
-    if response != new_response:
-        print(response)
-    response = new_response
+try:
+    while True:
+        new_response = s.readline()
+        if response != new_response:
+            print(response)
+        response = new_response
+except KeyboardInterrupt:
+    s.close()
 
     
 
