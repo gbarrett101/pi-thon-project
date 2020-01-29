c4ccdea2c451d751d4483e892a23f2a2b47ecb16
Zamora-404 <lzamora@mit.edu>
2020-01-28 20:38:25 -0500

    added LED control

---
+++ LedControl/LedSafe/LedSafe.ino
@@ -0,0 +1,54 @@
+#include <FastLED.h>
+#define LED_PIN         A1
+#define NUM_LEDS        10
+#define MAX_BRIGHTNESS  100
+#define DELAY           200
+
+int input = 1;
+
+void setup()
+{
+  Serial.begin(9600);
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness( MAX_BRIGHTNESS );
+
+}
+
+void loop()   /*----( LOOP: RUNS CONSTANTLY )----*/
+{
+  if (input == 1)
+  {
+    notSafe();
+  }
+  else
+  {
+    neutral();
+  }
+}
+
+
+void notSafe()
+{
+  FastLED.clear();
+  FastLED.setBrightness( MAX_BRIGHTNESS );
+  fill_solid( leds, NUM_LEDS, CRGB(255,0,0));
+
+  FastLED.show();
+  //delay(500);
+}
+
+void safe()
+{
+  FastLED.clear();
+  FastLED.setBrightness( MAX_BRIGHTNESS );
+  fill_solid( leds, NUM_LEDS, CRGB(0,255,0));
+
+  FastLED.show();
+}
+
+void neutral()
+{
+  FastLED.clear();
+  FastLED.show();
+
+}
