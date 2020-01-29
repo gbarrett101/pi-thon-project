71dc7431c023990e7610b311a937a6e6c2d49e60
Gabriel Barrett <gabrielbarrett@dhcp-18-21-166-238.dyn.mit.edu>
2020-01-28 21:30:00 -0500

    door control change

--- doorControl.py
+++ doorControl.py
@@ -22,6 +22,42 @@ def isSafe(distance, speed, threshold, doorPosition):
         return False
     return True
 
+def loop():
+    index = 0
+    while len(s.readline())>0:
+            print("loop going")
+            response = s.readline()
+            response = str(response, 'utf-8')
+            response = response.split(',')
+            speed = response[0]
+            distance = response[1]
+            signalStrength = response[2]
+            #code for the button that will initially be commented out
+            newButtonState = response[3]
+            speed = float(speed) #in cm/sec
+            distance = int(distance) #in cm
+            signalStrength = int(signalStrength)
+            if newButtonState == 0 and buttonState == 1:
+                buttonPushed = True
+                if isSafe(distance, speed, threshold, frontDoorDistance) :
+                    #maybe say something here
+                    buttonPushed = False
+                else:
+                    playsound('wait.mp3')
+            if not isSafe(distance, speed, threshold, frontDoorDistance) and index != 3:
+                #scenario where the car is not safe but previously before 
+                index = 3
+            elif index != 0 and isSafe(distance, speed, threshold, frontDoorDistance):
+                #scenario where the car is safe but it wasn't before
+                if buttonPushed:
+                    playsound('leftClear.mp3') #more audio feedback stuff
+                buttonPushed = False
+                index = 0
+            buttonState = newButtonState
+
+
+buttonState = 0
+newButtonState = 0
 buttonPushed = False #variable to keep track of the button state
 frontDoorDistance = 1
 rearDoorDistance = 2
@@ -29,16 +65,7 @@ threshold = 10
 
 
 try: #try statement to look out for keeyboard interupts 
-    while True:
-        response = s.readline()
-        response = response.split(',')
-        distance, speed = response
-        if not isSafe(distance, speed, threshold, frontDoorDistance):
-            #make front door red
-            pass
-        if not isSafe(distance, speed, threshold, rearDoorDistance):
-            #make rear door red
-            pass
+    loop()
 except KeyboardInterrupt:
     s.close()
 
