173022a7aebd29aba984349dc5e1500753451347
Gabriel Barrett <gabrielbarrett@dhcp-18-21-166-238.dyn.mit.edu>
2020-01-28 21:30:43 -0500

    door control change

--- doorControl.py
+++ doorControl.py
@@ -2,8 +2,11 @@ import serial
 import time
 
 #Open the serial port
-s = serial.Serial('/dev/ttyACM0', 9600) # change name, if needed
-s.open()
+s = serial.Serial('/dev/ttyUSB0', 115200) # change name, if needed
+try:
+    s.open()
+except:
+    pass
 time.sleep(5) # the Arduino is reset after enabling the serial connection, therefore we have to wait some seconds
 
 def isSafe(distance, speed, threshold, doorPosition):
